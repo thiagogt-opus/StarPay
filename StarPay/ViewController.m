f1febebf786ebf253a3f2cd6193939ae0249c844
thiagogt-opus <thiago.toledo@opus-software.com.br>
2016-02-03 23:18:13 -0200

    Adicionado código base analitycs
    Verifica as telas trocadas pelo viewController base

--- StarPay/ViewController.m
+++ StarPay/ViewController.m
@@ -8,11 +8,30 @@
 
 #import "ViewController.h"
 
+#import <Google/Analytics.h>
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    NSString *name = [NSString stringWithFormat:@"Pattern~%@", self.title];
+    
+    // The UA-XXXXX-Y tracker ID is loaded automatically from the
+    // GoogleService-Info.plist by the `GGLContext` in the AppDelegate.
+    // If you're copying this to an app just using Analytics, you'll
+    // need to configure your tracking ID here.
+    // [START screen_view_hit_objc]
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIScreenName value:name];
+    [tracker send:[[GAIDictionaryBuilder createScreenView] build]];
+    // [END screen_view_hit_objc]
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
